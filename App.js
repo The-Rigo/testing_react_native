d05395f0e725435304e473d3a3be40880bd1f8e7
The-Rigo <rodri.ledezma9483@gmail.com>
2021-06-04 12:33:04 -0400

    textInput

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [user, setUser]=useState("Alan")
@@ -11,7 +11,22 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.mytext}>{user} is eating {fruit.name}, which cost ${fruit.price}.</Text>
+      <Text style={styles.mytext}>{user} is eating {fruit.name}, 
+      which cost ${fruit.price}.</Text>
+      <TextInput style={styles.textinput} 
+        placeholder="Enter the username"
+        onChange = {e=>setUser(e.target.value)}
+      />
+      <TextInput style={styles.textinput} 
+        multiline
+        placeholder="Enter the fruit name"
+        onChange = {e=>setFruit({...fruit, name: e.target.value})}
+      />
+      <TextInput style={styles.textinput} 
+        keyboardType="numeric"
+        placeholder="Enter the fruit price"
+        onChange = {e=>setFruit({...fruit, price: e.target.value})}
+      />
       <View style ={styles.buttonstyle}>
         <Button title = "Change"
         onPress={pressHandler}
@@ -28,10 +43,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textinput:{
+    borderWidth:1,
+    borderColor:'black',
+    width:300,
+    height:50,
+    padding:10,
+    margin:10
+  },
   buttonstyle:{
     marginTop: 30
   },
   mytext:{
-    fontSize:40
+    fontSize:20
   }
 });
